0d457b6a2a3c9d6a32dfbe280d04102d424d93ca
“FedeAltava” <fedealtava1992@gmail.com>
2024-09-18 20:12:30 +0200

    cambios de añadir personas

--- src/app/fede-componente/fede-componente.component.ts
+++ src/app/fede-componente/fede-componente.component.ts
@@ -13,5 +13,12 @@ export class FedeComponenteComponent {
     {name:'Gabimon',type:'reptil'},
     {name:'Danimon',type:'rata'},
     {name:'aronmon',type:'nigga'}
-  ]
+  ];
+
+  tiposPersona: string[] = ['Reptil','rata','nigga'];
+
+  addPersonas(personas :{name: string, type: string}){
+    this.fedeLista.push(personas);
+  }
 }
+
